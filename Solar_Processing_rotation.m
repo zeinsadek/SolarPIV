226a56e4f52772f84fa2b044aea1e0360dba29c0
sadek <sadek@pdx.edu>
2025-02-22 15:48:50 -0800

    updates

---
+++ Solar_Processing_rotation.m
@@ -0,0 +1,229 @@
+% Solar Perspective Projected PIV
+
+clc
+clear all
+close all
+
+%% Setup
+
+% Case name: B45 is beta 45 deg
+cs = 'B45';
+means = load(strcat('./',cs,'.mat'));
+means = means.output;
+
+% Parameters
+U_inf = 4;
+U_inf_sq = U_inf^2;
+
+% Means
+U = means.u./U_inf;
+V = means.w./U_inf;
+W = means.v./U_inf;
+
+% Dispersive velcoity
+Ud = means.ud./U_inf;
+Vd = means.vd./U_inf;
+Wd = means.wd./U_inf;
+
+%% Coordinates (2D mesh)
+
+% PIV coordinates (2D)
+ZZ = means.X;
+YY = means.Y;
+
+% PIV coordinates (1D)
+z = ZZ(1,:);
+y = YY(:,1);
+
+% Grid spacing in y and z
+dyz = abs(y(1)-y(2)); 
+
+% Building the x coodrinate 
+x = 0:dyz:170*dyz; 
+
+% Building the 3D mesh
+[ZZZ,YYY,XXX] = meshgrid(z,y,x); 
+
+
+%% Coordinates (3D mesh)
+
+% Rotating to the new coordinates
+Udold = reshape(Ud, 1, []);
+Vdold = reshape(Vd, 1, []);
+Wdold = reshape(Wd, 1, []);
+
+XXXold = reshape(XXX, 1, []);
+YYYold = reshape(YYY, 1, []);
+ZZZold = reshape(ZZZ, 1, []);
+
+[temp,~,~] = AxelRot([Udold;Vdold;Wdold], -45, [0 1 0]', []);
+
+Udnew = reshape(temp(1,:), [length(y), length(z)]);
+Vdnew = reshape(temp(2,:), [length(y), length(z)]);
+Wdnew = reshape(temp(3,:), [length(y), length(z)]);
+
+[temp,~,~] = AxelRot([XXXold;YYYold;ZZZold], -45, [0 1 0]', []);
+
+XXXnew = reshape(temp(1,:), [length(y), length(z), length(x)]);
+YYYnew = reshape(temp(2,:), [length(y), length(z), length(x)]);
+ZZZnew = reshape(temp(3,:), [length(y), length(z), length(x)]);
+
+%% Rebuilding velocity vectors
+
+% Building the 3d velocity matrises
+Ud3d = 0*XXX;
+Vd3d = 0*XXX;
+Wd3d = 0*XXX;
+Vord3d = 0*XXX;
+
+
+% Building the 3D velocity matrises 
+for i=1:length(x)
+
+    Ud3d(:,1:end-(i-1),i) = Udnew(:,i:end);
+    Ud3d(:,end-(i-1)+1:end,i) = Udnew(:,1:i-1);
+
+    Vd3d(:,1:end-(i-1),i) = Vdnew(:,i:end);
+    Vd3d(:,end-(i-1)+1:end,i) = Vdnew(:,1:i-1);
+
+    Wd3d(:,1:end-(i-1),i) = Wdnew(:,i:end);
+    Wd3d(:,end-(i-1)+1:end,i) = Wdnew(:,1:i-1);
+end
+
+% Extracting the velocities of the rotated plane 
+Udrot = zeros(length(y), length(x));
+Vdrot = zeros(length(y), length(x));
+Wdrot = zeros(length(y), length(x));
+
+
+%Extracting the tilted plane from the rotated matrix 
+
+for i=1:length(x)
+
+    Udrot(:,i)=Ud3d(:,i,i);
+    Vdrot(:,i)=Vd3d(:,i,i);
+    Wdrot(:,i)=Wd3d(:,i,i);
+
+    % XXXrot(:,i)=XXXnew(:,i,i);
+    % YYYrot(:,i)=YYYnew(:,i,i);
+    % ZZZrot(:,i)=ZZZnew(:,i,i);
+     YYrot(:,i)=YYYnew(:,i,i);
+     ZZrot(:,i)=ZZZnew(:,i,i);
+end
+
+
+
+
+
+%% Dispersive Velocity Plots Rotated
+
+% kernel = 25;
+% Wd = juliaan_smooth(Wd, kernel);
+% Vd = juliaan_smooth(Vd, kernel);
+
+Reference_Velocity=0.05;
+Scale=double(max(max(sqrt(Vd.^2 + Wd.^2 )))/Reference_Velocity);
+
+ax = figure();
+vn=2;
+%t  = tiledlayout(1,3);
+% sgtitle(inpt_name, 'interpreter', 'none')
+%l_Du = 0.2;
+l_Du = 0.05;
+
+nexttile()
+contourf(ZZrot, YYrot, Udrot, 500, 'linestyle', 'none')
+colormap('redblue')
+axis equal
+hold on
+xlim([-150,150])
+ylim([-100,100])
+% xlim([-x_crop, x_crop])
+% ylim([0, y_crop])
+colorbar()
+title('$<\overline{u}> \,/ U_{\infty}^2$', 'Interpreter', 'latex','FontSize', 20)
+clim([-l_Du l_Du]);
+quiver(ZZrot(1:vn:end,1:vn:end),YYrot(1:vn:end,1:vn:end),Wdrot(1:vn:end,1:vn:end),Vdrot(1:vn:end,1:vn:end),Scale,'color',[0 0 0]);
+ylabel('$y/S_y$', 'Interpreter', 'latex')
+xlabel('$z/S_y$', 'Interpreter', 'latex')
+xlabel('z','FontSize', 20)
+ylabel('y','FontSize', 20)
+
+print(gcf,'Dew.svg','-dsvg','-r1200');  
+%exportgraphics(gcf,strcat(cs,'_rot.png'),'Resolution',3000) 
+
+%out_path = '/Users/katietaylor/Library/Mobile Documents/com~apple~CloudDocs/Research/PIV/Solar/Results/figures/Velocity';
+%exportgraphics(ax, fullfile(out_path, 'Dispersive Velocity_B0.png'), 'Resolution', 200);
+
+%{
+%% plotting in 3D
+
+figure ()
+
+for i=1:10
+
+    p=patch(isosurface(ZZZ,YYY,XXX,Ud3d,-0.5*i/1000));
+    f = i/10; % fraction number
+    p.FaceColor = [1 f 1-f];
+    p.EdgeColor = 'none';
+    p.FaceAlpha = 1-f;
+    axis tight
+    camlight 
+    lighting gouraud
+end
+
+daspect([1 1 0.1])
+
+view(0,91)
+camroll(180)
+
+for i=1:36
+   camorbit(1,1,'camera')
+   drawnow
+   pause(0.25)
+end
+% view(100,15)
+% camroll(90)
+%}
+
+%% Dispersive Velocity Plots
+
+% kernel = 25;
+% Wd = juliaan_smooth(Wd, kernel);
+% Vd = juliaan_smooth(Vd, kernel);
+
+Reference_Velocity=0.05;
+Scale=double(max(max(sqrt(Vd.^2 + Wd.^2 )))/Reference_Velocity);
+
+ax = figure();
+%t  = tiledlayout(1,3);
+% sgtitle(inpt_name, 'interpreter', 'none')
+%l_Du = 0.2;
+l_Du = 0.05;
+vn=2;
+
+nexttile()
+contourf(ZZ, YY, -Ud, 500, 'linestyle', 'none')
+colormap('redblue')
+axis equal
+hold on
+xlim([-200,150])
+ylim([-100,100])
+% xlim([-x_crop, x_crop])
+% ylim([0, y_crop])
+colorbar()
+title('$<\overline{u}> \,/ U_{\infty}^2$', 'Interpreter', 'latex','FontSize', 20)
+clim([-l_Du l_Du]);
+quiver(ZZ(1:vn:end,1:vn:end),YY(1:vn:end,1:vn:end),Wd(1:vn:end,1:vn:end),Vd(1:vn:end,1:vn:end),Scale,'color',[0 0 0]);
+ylabel('$y/S_y$', 'Interpreter', 'latex')
+xlabel('$z/S_y$', 'Interpreter', 'latex')
+xlabel('z','FontSize', 20)
+ylabel('y','FontSize', 20)
+%patch([-1.2 -1.2 1.02 1.02],[-0.02 0 0 -0.02 ],'k')
+%patch([-200 -200 150 150],[-105 -100 -100 -105 ],'k')
+
+%exportgraphics(gcf,strcat(cs,'.png'),'Resolution',3000) 
+print(gcf,'Nor.svg','-dsvg','-r1200');  
+
+%out_path = '/Users/katietaylor/Library/Mobile Documents/com~apple~CloudDocs/Research/PIV/Solar/Results/figures/Velocity';
+%exportgraphics(ax, fullfile(out_path, 'Dispersive Velocity_B0.png'), 'Resolution', 200);
---
+++ Solar_Processing_rotation_225.m
@@ -0,0 +1,285 @@
+clc
+clear all
+%close all
+addpath("/Users/katietaylor/Library/Mobile Documents/com~apple~CloudDocs/Research/PIV/Solar/Solar_Functions")
+
+project_path   = '/Users/katietaylor/Library/Mobile Documents/com~apple~CloudDocs/Research/PIV/Solar/Results/';
+inpt_name      = 'D_SS_NOM_S15_H3_A045_B225_PIVMID_UH_REMAP';
+mean_file      = strcat(project_path, 'means'  , '/', inpt_name, '_MEANS.mat');
+
+means = load(mean_file);
+means = means.output;
+
+
+%parameters
+
+U_inf = 4;
+U_inf_sq = U_inf^2;
+S_y = 150;
+vn = 5;
+x_crop = 1.5;
+y_crop = 1;
+
+%Variables
+% Coordinates (2D mesh)
+ZZ=(means.X);
+YY=(means.Y);
+
+z = ZZ(1,:);
+y = YY(:,1);
+
+dyz=abs(y(2)-y(1)); %grid spacing in y and z
+
+x=0:dyz:170*dyz; %building the x coodrinate 
+
+
+% Means
+U = means.u./U_inf;
+V = means.w./U_inf;
+W = means.v./U_inf;
+
+thld_V = min(V) + range(V)*0.001;
+thld_W = min(W) + range(W)*0.001;
+acc_V = V >= thld_V;
+acc_W = W >= thld_W;
+
+% Velocity profiles, average over spanwise direction (y)
+Uz=means.uz./U_inf;
+Vz=means.vz./U_inf;
+Wz=means.wz./U_inf;
+
+% Dispersive velcoity
+Ud=means.ud./U_inf;
+Vd=means.vd./U_inf;
+Wd=means.wd./U_inf;
+
+
+[ZZZ,YYY,XXX]=meshgrid(z,y,x); %Building the 3D mesh
+
+
+% Normal Turbuluent Stresses
+uu = means.uu./U_inf_sq;
+vv = means.vv./U_inf_sq;
+ww = means.ww./U_inf_sq;
+
+% Shear turbulent Stresses
+uv = means.uv./U_inf_sq;
+uw = means.uw./U_inf_sq;
+vw = means.vw./U_inf_sq;
+
+% Normal Dispersive stresses
+uud=means.uud./U_inf_sq;
+vvd=means.vvd./U_inf_sq;
+wwd=-means.wwd./U_inf_sq;
+
+% Shear Dispersive stresses
+uvd=means.uvd./U_inf_sq;
+uwd=-means.uwd./U_inf_sq;
+vwd=means.vwd./U_inf_sq;
+
+
+%Rotating to the new coordinates
+
+Udold=reshape(Ud,1,[]);
+Vdold=reshape(Vd,1,[]);
+Wdold=reshape(Wd,1,[]);
+
+XXXold=reshape(XXX,1,[]);
+YYYold=reshape(YYY,1,[]);
+ZZZold=reshape(ZZZ,1,[]);
+
+[temp,R,t]=AxelRot([Udold;Vdold;Wdold],45,[0 1 0]',[]');
+
+Udnew=reshape(temp(1,:),[length(y),length(z)]);
+Vdnew=reshape(temp(2,:),[length(y),length(z)]);
+Wdnew=reshape(temp(3,:),[length(y),length(z)]);
+
+[temp,R,t]=AxelRot([XXXold;YYYold;ZZZold],45,[0 1 0]',[]);
+
+XXXnew=reshape(temp(1,:),[length(y),length(z),length(x)]);
+YYYnew=reshape(temp(2,:),[length(y),length(z),length(x)]);
+ZZZnew=reshape(temp(3,:),[length(y),length(z),length(x)]);
+
+%%%%%%%
+
+%Building the 3d velocity matrises
+Ud3d=0*XXX;
+Vd3d=0*XXX;
+Wd3d=0*XXX;
+Vord3d=0*XXX;
+
+
+% %wrong smear
+% for i=1:length(x)
+% 
+%     Ud3d(:,i:end,i)=Udnew(:,1:end-(i-1));
+%     Ud3d(:,1:i-1,i)=Udnew(:,end-(i-1)+1:end);
+% 
+%     Vd3d(:,i:end,i)=Vdnew(:,1:end-(i-1));
+%     Vd3d(:,1:i-1,i)=Vdnew(:,end-(i-1)+1:end);
+% 
+%     Wd3d(:,i:end,i)=Wdnew(:,1:end-(i-1));
+%     Wd3d(:,1:i-1,i)=Wdnew(:,end-(i-1)+1:end);
+% 
+%     % Vord3d(:,i:end,i)=vord(:,1:end-(i-1));
+%     % Vord3d(:,1:i-1,i)=vord(:,end-(i-1)+1:end);
+% 
+% end
+
+for i = 1:length(x)
+    % First part: Take data from the first i columns of Udnew, Vdnew, Wdnew
+    Ud3d(:, 1:i, i) = Udnew(:, end-(i-1):end); 
+    Vd3d(:, 1:i, i) = Vdnew(:, end-(i-1):end); 
+    Wd3d(:, 1:i, i) = Wdnew(:, end-(i-1):end); 
+    
+    % Second part: Take data from columns i+1 to the end of Udnew, Vdnew, Wdnew
+    Ud3d(:, i+1:end, i) = Udnew(:, 1:end-(i));
+    Vd3d(:, i+1:end, i) = Vdnew(:, 1:end-(i)); 
+    Wd3d(:, i+1:end, i) = Wdnew(:, 1:end-(i)); 
+end
+
+% % Building the 3D velocity matrises 
+% for i=1:length(x)
+% 
+%     Ud3d(:,1:end-(i-1),i)=Udnew(:,i:end);
+%     Ud3d(:,end-(i-1)+1:end,i)=Udnew(:,1:i-1);
+% 
+%     Vd3d(:,1:end-(i-1),i)=Vdnew(:,i:end);
+%     Vd3d(:,end-(i-1)+1:end,i)=Vdnew(:,1:i-1);
+% 
+%     Wd3d(:,1:end-(i-1),i)=Wdnew(:,i:end);
+%     Wd3d(:,end-(i-1)+1:end,i)=Wdnew(:,1:i-1);
+% end
+
+%Extracting the velocities of the rotated plane 
+Udrot=zeros(length(y),length(x));
+Vdrot=zeros(length(y),length(x));
+Wdrot=zeros(length(y),length(x));
+
+
+%Extracting the tilted plane from the rotated matrix 
+
+for i=1:length(x)
+
+    Udrot(:,i)=Ud3d(:,i,i);
+    Vdrot(:,i)=Vd3d(:,i,i);
+    Wdrot(:,i)=Wd3d(:,i,i);
+
+    % XXXrot(:,i)=XXXnew(:,i,i)
+     YYrot(:,i)=YYYnew(:,i,i);
+     ZZrot(:,i)=ZZZnew(:,i,i);
+end
+
+
+%% Dispersive Velocity Plots Rotated
+
+kernel = 25;
+Wd = juliaan_smooth(Wd, kernel);
+Vd = juliaan_smooth(Vd, kernel);
+
+Reference_Velocity=0.05;
+Scale=double(max(max(sqrt(Vd.^2 + Wd.^2 )))/Reference_Velocity);
+
+ax = figure();
+vn=2;
+%t  = tiledlayout(1,3);
+% sgtitle(inpt_name, 'interpreter', 'none')
+%l_Du = 0.2;
+l_Du = 0.05;
+
+nexttile()
+contourf(ZZrot, YYrot, Udrot, 500, 'linestyle', 'none')
+colormap('redblue')
+axis equal
+hold on
+xlim([-150,150])
+ylim([-100,100])
+% xlim([-x_crop, x_crop])
+% ylim([0, y_crop])
+colorbar()
+title('$<\overline{u}> \,/ U_{\infty}^2$', 'Interpreter', 'latex','FontSize', 20)
+% clim([-l_Du l_Du]);
+quiver(ZZrot(1:vn:end,1:vn:end),YYrot(1:vn:end,1:vn:end),Wdrot(1:vn:end,1:vn:end),Vdrot(1:vn:end,1:vn:end),2*Scale,'color',[0 0 0]);
+ylabel('$y/S_y$', 'Interpreter', 'latex')
+xlabel('$z/S_y$', 'Interpreter', 'latex')
+xlabel('z','FontSize', 20)
+ylabel('y','FontSize', 20)
+
+print(gcf,'Dew.svg','-dsvg','-r1200');  
+%exportgraphics(gcf,strcat(cs,'_rot.png'),'Resolution',3000) 
+
+%out_path = '/Users/katietaylor/Library/Mobile Documents/com~apple~CloudDocs/Research/PIV/Solar/Results/figures/Velocity';
+%exportgraphics(ax, fullfile(out_path, 'Dispersive Velocity_B0.png'), 'Resolution', 200);
+
+%{
+%% plotting in 3D
+
+figure ()
+
+for i=1:10
+
+    p=patch(isosurface(ZZZ,YYY,XXX,Ud3d,-0.5*i/1000));
+    f = i/10; % fraction number
+    p.FaceColor = [1 f 1-f];
+    p.EdgeColor = 'none';
+    p.FaceAlpha = 1-f;
+    axis tight
+    camlight 
+    lighting gouraud
+end
+
+daspect([1 1 0.1])
+
+view(0,91)
+camroll(180)
+
+for i=1:36
+   camorbit(1,1,'camera')
+   drawnow
+   pause(0.25)
+end
+% view(100,15)
+% camroll(90)
+%}
+
+%% Dispersive Velocity Plots
+
+% kernel = 25;
+% Wd = juliaan_smooth(Wd, kernel);
+% Vd = juliaan_smooth(Vd, kernel);
+
+Reference_Velocity=0.05;
+Scale=double(max(max(sqrt(Vd.^2 + Wd.^2 )))/Reference_Velocity);
+
+ax = figure();
+%t  = tiledlayout(1,3);
+% sgtitle(inpt_name, 'interpreter', 'none')
+%l_Du = 0.2;
+l_Du = 0.05;
+vn=2;
+
+nexttile()
+contourf(ZZ, YY, -Ud, 500, 'linestyle', 'none')
+colormap('redblue')
+axis equal
+hold on
+xlim([-200,150])
+ylim([-100,100])
+% xlim([-x_crop, x_crop])
+% ylim([0, y_crop])
+colorbar()
+title('$<\overline{u}> \,/ U_{\infty}^2$', 'Interpreter', 'latex','FontSize', 20)
+clim([-l_Du l_Du]);
+quiver(ZZ(1:vn:end,1:vn:end),YY(1:vn:end,1:vn:end),Wd(1:vn:end,1:vn:end),Vd(1:vn:end,1:vn:end),Scale,'color',[0 0 0]);
+ylabel('$y/S_y$', 'Interpreter', 'latex')
+xlabel('$z/S_y$', 'Interpreter', 'latex')
+xlabel('z','FontSize', 20)
+ylabel('y','FontSize', 20)
+%patch([-1.2 -1.2 1.02 1.02],[-0.02 0 0 -0.02 ],'k')
+%patch([-200 -200 150 150],[-105 -100 -100 -105 ],'k')
+
+%exportgraphics(gcf,strcat(cs,'.png'),'Resolution',3000) 
+print(gcf,'Nor.svg','-dsvg','-r1200');  
+
+%out_path = '/Users/katietaylor/Library/Mobile Documents/com~apple~CloudDocs/Research/PIV/Solar/Results/figures/Velocity';
+%exportgraphics(ax, fullfile(out_path, 'Dispersive Velocity_B0.png'), 'Resolution', 200);
